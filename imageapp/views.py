a81da0426f3eaaab43a2a515ee92729984aa00f9
eduardo <dudumelgaco@hotmail.com>
2019-09-01 23:53:40 -0300

    Add views, urls and change settings properties

--- imageapp/views.py
+++ imageapp/views.py
@@ -1,3 +1,69 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import JsonResponse
 from django.shortcuts import render
 
-# Create your views here.
+from django.views import View
+from django.views.generic.edit import FormView
+from django.views.generic.list import MultipleObjectMixin, ListView
+
+from .forms import PhotoForm
+from .models import Photo
+
+class ListPhotosView(FormView, MultipleObjectMixin):
+    form_class = PhotoForm
+    template_name = 'home_page.html'
+    success_url = '/'
+    paginate_by = 6
+
+    def form_valid(self, form):
+        if form.is_valid():
+            form.save()
+        return super(ListPhotosView, self).form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        object_list = Photo.objects.filter(approved=True)
+        order_param = self.request.GET.get('order')
+        if order_param:
+            object_list = object_list.order_by('-'+order_param)
+        return super(ListPhotosView, self).get_context_data(object_list=object_list, **kwargs)
+
+class ApprovePhotoListView(LoginRequiredMixin, ListView):
+    login_url = '/login'
+    model = Photo
+    paginate_by = 6
+    template_name = 'imageapp/approve-image.html'
+
+    def get_queryset(self):
+        return Photo.objects.filter(approved=False)
+
+    def post(self, request, *args, **kwargs):
+        approve_param_id = request.POST.get('approve', None)
+        delete_param_id = request.POST.get('delete', None)
+        if approve_param_id:
+            photo_to_approve = Photo.objects.filter(id=approve_param_id).first()
+            if photo_to_approve:
+                photo_to_approve.approved = True
+                photo_to_approve.save()
+        elif delete_param_id:
+            photo_to_delete = Photo.objects.filter(id=delete_param_id).first()
+            if photo_to_delete:
+                photo_to_delete.delete()
+
+        object_list = self.get_queryset()
+        return render(request, self.template_name, context={'object_list': object_list})
+
+
+class LikePhotoView(View):
+    def post(self, request):
+        if request.is_ajax():
+            photo_id = request.POST.get('image_id')
+            photo = Photo.objects.filter(id=photo_id).first()
+            if photo:
+                photo.likes += 1
+                photo.save()
+                json_data = {
+                    "image": photo.id,
+                    "likes": photo.likes
+                }
+                return JsonResponse(json_data, status=200)
+        return JsonResponse({'error': 'Not allowed.'}, status=400)
--- imagegallery/settings.py
+++ imagegallery/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Apps
+    'imageapp',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'imagegallery.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,7 +117,14 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
