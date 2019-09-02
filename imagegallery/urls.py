abaf663d774bc75d8fbc69493ae9ed93460e326c
eduardo <dudumelgaco@hotmail.com>
2019-09-02 19:19:36 -0300

    Run Black trough imagegallery files

--- imagegallery/urls.py
+++ imagegallery/urls.py
@@ -6,8 +6,8 @@ from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include(("imageapp.urls", 'imageapp'), namespace='imageapp')),
-    path('admin/', admin.site.urls),
-    path('login/',  auth_views.LoginView.as_view(), name='login'),
-    path('logout/',  auth_views.LogoutView.as_view(), name='logout'),
+    path("", include(("imageapp.urls", "imageapp"), namespace="imageapp")),
+    path("admin/", admin.site.urls),
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
