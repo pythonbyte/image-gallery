6babe99d052b42a452727495339d697cec084776
eduardo <dudumelgaco@hotmail.com>
2019-09-02 11:15:55 -0300

    Add aws config to connect s3 bucket

--- imagegallery/settings.py
+++ imagegallery/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from imagegallery.aws.conf import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =  os.environ.get('SECRET_KEY')
+SECRET_KEY =  'uynthka!_ba+@s81gkk*xxqjt7%yg(_&139lx66--zxjl+n%kh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
 
     # Apps
     'imageapp',
+    'storages',
 ]
 
 MIDDLEWARE = [
