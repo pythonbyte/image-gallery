cff42eeb220a14ee1b0d52f7a2d66a3fd01fec22
eduardo <dudumelgaco@hotmail.com>
2019-09-02 19:54:27 -0300

    Fix env vars of database and secret key and add Dockerfile

--- imagegallery/settings.py
+++ imagegallery/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =  'uynthka!_ba+@s81gkk*xxqjt7%yg(_&139lx66--zxjl+n%kh'
+SECRET_KEY =  os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
